962e7813b22a3c9ed6bec313400da32aa6daac12
Hussein Elged <elged194@gmail.com>
2024-04-23 11:59:39 -0700

    first commit

---
+++ active.js
@@ -0,0 +1,48 @@
+const nextButton = document.querySelector(".btn-next");
+const prevButton = document.querySelector(".btn-prev");
+const steps = document.querySelectorAll(".step");
+const form_step = document.querySelectorAll(".form-step");
+let active = 1;
+
+nextButton.addEventListener("click", () => {
+  active++;
+  if (active > steps.length) {
+    active = steps.length;
+  }
+  updateProgress();
+});
+
+
+prevButton.addEventListener("click", () => {
+  active--;
+  if (active < 1) {
+    active = 1;
+  }
+  updateProgress();
+});
+
+const updateProgress = () => {
+  console.log("steps.length =>" + steps.length);
+  console.log("active =>" + active);
+
+  steps.forEach((step, i) => {
+    if (i == (active - 1)) {
+      step.classList.add("active");
+      form_step[i].classList.add("active");
+      console.log("i=>" + i);
+    } else {
+      step.classList.remove("active");
+      form_step[i].classList.remove("active");
+    }
+  });
+
+
+  if (active === 1) {
+    prevButton.disabled = true;
+  } else if (active === steps.length) {
+    nextButton.disabled = true;
+  } else {
+    prevButton.disabled = false;
+    nextButton.disabled = false;
+  }
+};
